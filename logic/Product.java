d81e997cd6c34033e016464f6ec4c2f643fb8728
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-03 15:32:28 +0000

    Prepare interface definitions for logic package

---
+++ logic/Product.java
@@ -0,0 +1,21 @@
+package logic;
+
+public interface Product {
+    String getProductNumber();
+    void setProductNumber(String value);
+
+    String getDescription();
+    void setDescription(String value);
+
+    double getPrice();
+    void setPrice(double value);
+
+
+    /**
+     * Gets the current number of this product in stock.
+     *
+     * @return Number of product in stock.
+     */
+    int getQuantity();
+    void setQuantity(int value);
+}
