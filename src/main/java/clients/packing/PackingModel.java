ecf79327528b48fdf0418ff45bf70c0cec0e7723
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-12 19:17:18 +0000

    Make project structure follow Maven best practices I'm doing this to set up testing for package-private classes

---
+++ src/main/java/clients/packing/PackingModel.java
@@ -0,0 +1,179 @@
+package clients.packing;
+
+import debug.DEBUG;
+import logic.*;
+
+import java.util.Currency;
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.Observable;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static logic.Order.State;
+
+/**
+ * Implements the Model of the warehouse packing client
+ */
+public class PackingModel extends Observable {
+    private final AtomicReference<Order> currentOrder = new AtomicReference<>();
+
+    private OrderProcessor orderProcessor = null;
+    private ProductReader productReader = null;
+
+    private final Semaphore packingWorker = new Semaphore();
+
+    /**
+     * Construct the model of the warehouse Packing client
+     *
+     * @param mf The factory to create the connection objects
+     */
+    public PackingModel(LogicFactory mf) {
+        try {
+            orderProcessor = mf.getOrderProcessor();
+            productReader = mf.getProductReader();
+        } catch (Exception e) {
+            DEBUG.error("CustomerModel.constructor\n%s", e.getMessage());
+        }
+
+        currentOrder.set(null);
+        // Start a background check to see when a new order can be packed
+        new Thread(this::checkForNewOrder).start();
+        new Thread(this::refreshOrderData).start();
+    }
+
+    /**
+     * Semaphore used to only allow 1 order
+     * to be packed at once by this person
+     */
+    static class Semaphore {
+        private boolean held = false;
+
+        /**
+         * Claim exclusive remote.access
+         *
+         * @return true if claimed else false
+         */
+        public synchronized boolean claim() {
+            return !held && (held = true);
+        }
+
+        /**
+         * Free the lock
+         */
+        public synchronized void free() {
+            assert held;
+            held = false;
+        }
+    }
+
+    /**
+     * Method run in a separate thread to check if there
+     * is a new order waiting to be packed and we have
+     * nothing to do.
+     */
+    private void checkForNewOrder() {
+        while (true) {
+            try {
+                boolean isFree = packingWorker.claim();
+                if (isFree) {
+                    Order sb = orderProcessor.popOrder();
+                    String prompt = "";
+                    if (sb != null) {
+                        currentOrder.set(sb);
+                        prompt = "Bought Receipt";
+                    } else {
+                        packingWorker.free();
+                        prompt = "";
+                    }
+
+                    setChanged();
+                    notifyObservers(prompt);
+                }
+
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void refreshOrderData() {
+        while (true) {
+            try {
+                orderProcessor.requestDataRefresh();
+
+                Thread.sleep(10000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Return the Basket of products that are to be picked
+     *
+     * @return the basket
+     */
+    public Order getCurrentOrder() {
+        return currentOrder.get();
+    }
+
+    /**
+     * Process a packed Order
+     */
+    public void packOrder() {
+        String prompt = "";
+
+        Order order = this.currentOrder.get();
+        if (order != null) {
+            this.currentOrder.set(null);
+            order.setState(State.BEING_PACKED);
+            orderProcessor.addOrderToQueue(order);
+            prompt = "";
+            packingWorker.free();
+        } else {
+            prompt = "No order";
+        }
+        setChanged();
+        notifyObservers(prompt);
+
+        setChanged();
+        notifyObservers(prompt);
+    }
+
+    public String getOrderDescription() {
+        Locale locale = Locale.UK;
+        StringBuilder stringBuilder = new StringBuilder(256);
+        Formatter formatter = new Formatter(stringBuilder, locale);
+        String currencySymbol = (Currency.getInstance(locale)).getSymbol();
+        double total = 0.00;
+        Order order = this.currentOrder.get();
+
+        if (order.getOrderNumber() != 0)
+            formatter.format("Order number: %03d\n", order.getOrderNumber());
+
+        if (!order.isEmpty()) {
+            for (Order.Item item : order.getAllItems()) {
+                Product product = productReader.getProductDetails(item.getProductNumber());
+                formatter.format("%-7s", product.getProductNumber());
+                formatter.format("%-14.14s ", product.getDescription());
+                formatter.format("(%3d) ", item.getQuantity());
+                formatter.format("%s%7.2f", currencySymbol, product.getPrice() * item.getQuantity());
+                formatter.format("\n");
+                total += product.getPrice() * item.getQuantity();
+            }
+
+            formatter.format("----------------------------\n");
+            formatter.format("Total                       ");
+            formatter.format("%s%7.2f\n", currencySymbol, total);
+            formatter.close();
+        }
+
+        return stringBuilder.toString();
+    }
+}
+
+
+
+
+
---
+++ src/main/java/clients/packing/PackingView.java
@@ -0,0 +1,92 @@
+package clients.packing;
+
+import logic.Order;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Implements the Packing view.
+ */
+
+public class PackingView implements Observer {
+    private static final String PACKED = "Packed";
+
+    private static final int HEIGHT = 300;
+    private static final int WIDTH = 400;
+
+    private final JLabel pageTitle = new JLabel();
+    private final JLabel promptLabel = new JLabel();
+    private final JTextArea messageOutput = new JTextArea();
+    private final JScrollPane messageScrollPane = new JScrollPane();
+    private final JButton packButton = new JButton(PACKED);
+
+    private PackingController controller = null;
+
+    /**
+     * Construct the view
+     *
+     * @param rootPane Window in which to construct
+     * @param x   x-coordinate of position of window on screen
+     * @param y   y-coordinate of position of window on screen
+     */
+    public PackingView(RootPaneContainer rootPane, int x, int y) {
+        // Content Pane
+        Container contentPane = rootPane.getContentPane();
+        Container rootWindow = (Container) rootPane;
+        contentPane.setLayout(null);
+        rootWindow.setSize(WIDTH, HEIGHT);
+        rootWindow.setLocation(x, y);
+
+        Font monospaceFont = new Font("Monospaced", Font.PLAIN, 12);
+
+        pageTitle.setBounds(110, 0, 270, 20);
+        pageTitle.setText("Packing Bought Order");
+        contentPane.add(pageTitle);
+
+        packButton.setBounds(16, 25, 80, 40);
+        packButton.addActionListener(
+                e -> controller.packOrder()
+        );
+        contentPane.add(packButton);
+
+        promptLabel.setBounds(110, 25, 270, 20);
+        promptLabel.setText("");
+        contentPane.add(promptLabel);
+
+        messageScrollPane.setBounds(110, 55, 270, 205);
+        messageOutput.setText("");
+        messageOutput.setFont(monospaceFont);
+        contentPane.add(messageScrollPane);
+
+        messageScrollPane.getViewport().add(messageOutput);
+        rootWindow.setVisible(true);
+    }
+
+    public void setController(PackingController controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * Update the view
+     *
+     * @param modelC The observed model
+     * @param arg    Specific args
+     */
+    @Override
+    public void update(Observable modelC, Object arg) {
+        PackingModel model = (PackingModel) modelC;
+        String message = (String) arg;
+        promptLabel.setText(message);
+
+        Order order = model.getCurrentOrder();
+        if (order != null) {
+            messageOutput.setText(controller.getOrderDescription());
+        } else {
+            messageOutput.setText("");
+        }
+    }
+}
+
