63ecded40e756c88d6efe49e8d3fd16ecb3df958
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-01-10 00:52:31 +0000

    Make FacadeRepositoryFactory customisable

--- src/test/java/facade/FacadeRepositoryFactory.java
+++ src/test/java/facade/FacadeRepositoryFactory.java
@@ -6,18 +6,38 @@ import remote.Repository;
 import remote.RepositoryFactory;
 
 public class FacadeRepositoryFactory implements RepositoryFactory {
+    private Repository<Order> orderRepository;
+    private Repository<Product> productRepository;
+    private Repository<Product> stockRepository;
+
+    public FacadeRepositoryFactory() {
+        orderRepository = new FacadeOrderRepository();
+    }
+
     @Override
     public Repository<Order> getOrderRepository() {
-        return new FacadeOrderRepository();
+        return orderRepository;
     }
 
     @Override
     public Repository<Product> getProductRepository() {
-        return new FacadeProductRepository();
+        return productRepository;
     }
 
     @Override
     public Repository<Product> getStockRepository() {
-        return new FacadeProductRepository();
+        return stockRepository;
+    }
+
+    public void setOrderRepository(Repository<Order> orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void setProductRepository(Repository<Product> productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void setStockRepository(Repository<Product> stockRepository) {
+        this.stockRepository = stockRepository;
     }
 }
